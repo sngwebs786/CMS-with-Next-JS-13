9227bb84d308bdf687ff63ced57d494313a6cb38
sngwebs786 <sngwebs786@gmail.com>
2023-06-17 14:20:51 +0500

    get image

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,28 +1,40 @@
 import { getData } from "../lib/contentful/getData";
 
 const Blogs = async ({ id }) => {
-  console.log(id);
+  let loading = true;
   var data;
   const res = await getData();
   res.items.map((item) => {
     if (item.fields.id == id) {
       data = item.fields;
+      loading = false;
     }
   });
-  // const blogid=res.items
-  // console.log("data: ", data);
   return (
     <>
-      <div>Blogs</div>
-      {!data ? (
+      {!loading ? (
         <>
-          <h1>Sorry no such blog found</h1>
+          <div>Blogs</div>
+          {!data ? (
+            <>
+              <h1>Sorry no such blog found</h1>
+            </>
+          ) : (
+            <>
+              <h1>Blog title : {data.blogTitle}</h1>
+              <p>Blog Description : {data.blogDescription}</p>
+              <img
+                src={`https:${data.blogImage.fields.file.url}`}
+                alt="image"
+                height={"400px"}
+                width={"400px"}
+              />
+            </>
+          )}
         </>
       ) : (
         <>
-          <h1>Blog title : {data.blogTitle}</h1>
-          <p>Blog Description : {data.blogDescription}</p>
-          {/* <h1>Blog title : {}</h1> */}
+          <h1>Loading...</h1>
         </>
       )}
     </>
